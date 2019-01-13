acc5547d1df9ed43fcde57998ef6b8917f109b2e
lhq <lhq@debian.localdomain>
2019-01-13 14:50:52 +0800

    add /docs to deploy

---
+++ docs/precache-manifest.4499476dbc268998af27ca05fb62dfe3.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ce9710183961770cc52",
+    "url": "/sim-cpu/static/js/runtime~main.7ce97101.js"
+  },
+  {
+    "revision": "e25a31dd308815e3833f",
+    "url": "/sim-cpu/static/js/main.e25a31dd.chunk.js"
+  },
+  {
+    "revision": "b0efac358531d5ec0b7f",
+    "url": "/sim-cpu/static/js/1.b0efac35.chunk.js"
+  },
+  {
+    "revision": "b64c7f50c8400827a44357cdd45f6489",
+    "url": "/sim-cpu/index.html"
+  }
+];
